ebfc3ea0ec11e42349b2348fa38661d64de61fe5
Alexandra <alexandra.blandon@gmail.com>
2025-10-17 17:15:17 -0400

    Removed node_modules...again. added controller and routes files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,16 @@
 import express from 'express'
-import as userController from '../controllers/userController.js';
+import userController from '../controllers/userController.js';
 
-const router = express.Router()length
+const router = express.Router()
 
 //route for /api/users
 router.route('/')
     .get(userController.getUsers)   
     .post(userController.createUser);
 
-// Routes for /api/users/:id
+// routes for /api/users/:id
 router.route('/:id')
-    .get(userController.getUserById) // READ: Get a single user by ID
-    // Note: No PATCH/PUT for users in this simple version, but DELETE is included.
-    .delete(userController.deleteUser); // DELETE: Delete a user
+    .get(userController.getUserById) // READ
+    .delete(userController.deleteUser); // delete a user
 
 export default router;
